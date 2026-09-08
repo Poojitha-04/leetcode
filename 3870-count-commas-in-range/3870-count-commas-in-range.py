cc16c05959a9870f49a86e6c5102976d69bcabe5
Poojitha-04 <82751694+Poojitha-04@users.noreply.github.com>
2026-09-07 22:02:49 -0400

    Time: 0 ms (100%), Space: 19.3 MB (46.53%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        if n<1000:
+            return 0
+        if n>=1000 and n<1000000:
+            return n-999
+       
+
+        
+    
+        
